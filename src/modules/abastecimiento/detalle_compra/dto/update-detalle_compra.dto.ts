b0b6bd6b36e17cd6bc735ff19527cb9b43f5f1e2
Patri4 <patrimendoza195@gmail.com>
2025-11-20 01:01:51 -0300

    Arquitectura de carpetas

--- src/modules/abastecimiento/detalle_compra/dto/update-detalle_compra.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDetalleCompraDto } from './create-detalle_compra.dto';
-
-export class UpdateDetalleCompraDto extends PartialType(CreateDetalleCompraDto) {}
--- src/modules/finanzas/fondos/dto/update-fondo.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFondoDto } from './create-fondo.dto';
-
-export class UpdateFondoDto extends PartialType(CreateFondoDto) {}
